de1557d9c5c188288d15a56a6e46771d15b318a9
tsar <tsar@tsar-netbook.(none)>
2012-01-31 14:54:43 +0400

    Coding, coding...

--- bash/life/gen_life.sh
+++ bash/life/gen_life.sh
@@ -10,6 +10,22 @@ N
 
 EOF
 
+for i in `seq 0 65535`
+do
+    echo -n "$i: "
+    for j in `seq 15 -1 0`
+    do
+        let "k=2**$j"
+        if [ $[i & k] -eq "$k" ]
+        then
+            echo -n "1"
+        else
+            echo -n "0"
+        fi
+    done
+    echo " b"
+done
+
 cat << EOF >> life.sh
 s/0011\n1011\n0010\n0000/aabb\nccdd\nmmmm\nnnnn/; tend
 s/0011\n1011\n0010\n0001/aabb\nccdd\nmmmm\nnnnn/; tend
