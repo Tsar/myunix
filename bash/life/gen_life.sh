f75237dd357287b8e6fe87d4a02cfc2c91a47d31
tsar <tsar@tsar-netbook.(none)>
2012-01-31 14:29:26 +0400

    Starting writing generator

--- bash/life/gen_life.sh
+++ bash/life/gen_life.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 
+cat << EOF > life.sh
+#!/bin/bash
+
 sed -e '
 N
 N
 N
 
+EOF
+
+cat << EOF >> life.sh
 s/0011\n1011\n0010\n0000/aabb\nccdd\nmmmm\nnnnn/; tend
 s/0011\n1011\n0010\n0001/aabb\nccdd\nmmmm\nnnnn/; tend
 s/aabb\nccdd\nmmmm\nnnnn/xxxx/; tend
+EOF
+
+cat << EOF >> life.sh
 
 :end
 ' field.txt
+EOF
