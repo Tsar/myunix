a5c696b5e16a16849ea39e7d800e2cb373287816
Ioann Volkov <volkov.ioann@gmail.com>
2012-04-09 11:46:06 +0400

    Update screenshot, test.c, add some tests to launch.sh

--- myalloc/test.c
+++ myalloc/test.c
@@ -4,7 +4,7 @@
 int main() {
     int n = 500;
     unsigned char* buf = (unsigned char*)malloc(500);
-/*
+
     int i;
     for (i = 0; i < 500; ++i) {
         buf[i] = i % 0xFF;
@@ -12,7 +12,7 @@ int main() {
     for (i = 500 - 1; i >= 0; --i) {
         printf("%d ", buf[i]);
     }
-*/
+
     free(buf);
 
     return 0;
