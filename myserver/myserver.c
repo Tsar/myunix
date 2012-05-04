4f331975f22d055f4f14d8e6aac5f8db47045cec
Ioann Volkov <volkov.ioann@gmail.com>
2012-05-04 15:55:24 +0400

    Small renames mostly

--- myserver/myserver.c
+++ myserver/myserver.c
@@ -14,31 +14,40 @@ void error(const char* msg) {
 }
 
 typedef struct {
+    int socketDescriptor;
+    pthread_t threadId;
+} TalkerThreadInfo;
+
+typedef struct {
     int portNumber;
     int ipv6;
     pthread_t threadId;
 } AcceptorThreadInfo;
 
-static void* threadAcceptor(void* acceptorArgs) {
-    AcceptorThreadInfo* aa = (AcceptorThreadInfo*)acceptorArgs;
+static void* threadTalker(void* talkerThreadInfo) {
+    TalkerThreadInfo* tti = (TalkerThreadInfo*)talkerThreadInfo;
+    
+}
 
-    int socketDescriptor = socket(aa->ipv6 ? AF_INET6 : AF_INET, SOCK_STREAM, 0);
+static void* threadAcceptor(void* acceptorThreadInfo) {
+    AcceptorThreadInfo* ati = (AcceptorThreadInfo*)acceptorThreadInfo;
+    int socketDescriptor = socket(ati->ipv6 ? AF_INET6 : AF_INET, SOCK_STREAM, 0);
     if (socketDescriptor < 0)
         error("ERROR: Could not create socket");
     struct sockaddr_in servAddr;
     struct sockaddr_in6 servAddr6;
-    if (aa->ipv6) {
+    if (ati->ipv6) {
         memset(&servAddr6, 0, sizeof(servAddr6));
         servAddr6.sin6_family = AF_INET6;
-        servAddr6.sin6_port = htons(aa->portNumber);
+        servAddr6.sin6_port = htons(ati->portNumber);
         servAddr6.sin6_addr = in6addr_any;
     } else {
         memset(&servAddr, 0, sizeof(servAddr));
         servAddr.sin_family = AF_INET;
         servAddr.sin_addr.s_addr = INADDR_ANY;
-        servAddr.sin_port = htons(aa->portNumber);
+        servAddr.sin_port = htons(ati->portNumber);
     }
-    if (bind(socketDescriptor, aa->ipv6 ? (struct sockaddr*)&servAddr6 : (struct sockaddr*)&servAddr, aa->ipv6 ? sizeof(servAddr6) : sizeof(servAddr)) < 0)
+    if (bind(socketDescriptor, ati->ipv6 ? (struct sockaddr*)&servAddr6 : (struct sockaddr*)&servAddr, ati->ipv6 ? sizeof(servAddr6) : sizeof(servAddr)) < 0)
         error("ERROR: Could not bind socket");
     if (listen(socketDescriptor, 10) < 0)
         error("ERROR: Could not start listening on socket");
@@ -46,10 +55,14 @@ static void* threadAcceptor(void* acceptorArgs) {
         struct sockaddr_in cliAddr;
         struct sockaddr_in6 cliAddr6;
         socklen_t cliAddrLen;
-        int acSocketDescriptor = accept(socketDescriptor, aa->ipv6 ? (struct sockaddr*)&cliAddr6 : (struct sockaddr*)&cliAddr, &cliAddrLen);
+        int acSocketDescriptor = accept(socketDescriptor, ati->ipv6 ? (struct sockaddr*)&cliAddr6 : (struct sockaddr*)&cliAddr, &cliAddrLen);
         if (acSocketDescriptor < 0)
             error("ERROR: Error on accepting");
-        
+        //TODO: here should be IP output
+        TalkerThreadInfo* tti = malloc(sizeof(TalkerThreadInfo));
+        tti->socketDescriptor = acSocketDescriptor;
+        if (pthread_create(&tti->threadId, NULL, threadTalker, (void*)tti) != 0)
+            error("ERROR: Could not create talker thread");
     }
 }
 
@@ -70,8 +83,7 @@ int main(int argc, char* argv[]) {
         if (pthread_join(atis[0 /* i */].threadId, NULL) != 0)
             error("ERROR: Could not join to acceptor thread");
     //}
-
-    free(atis);
+    //free(atis);
 
     return 0;
 }
